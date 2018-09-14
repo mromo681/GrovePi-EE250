ea0d208db714ea40deb1eeec7d982964069fe1f9
Martin <romoma@usc.edu>
2018-09-14 16:14:48 -0700

    LED TCP stuff

--- ee250/lab03/part3/ledClient.py
+++ ee250/lab03/part3/ledClient.py
@@ -2,5 +2,26 @@
 #
 # This code runs on yoru VM and sends requests to the Raspberry Pi to turn on 
 # and off the Grove LED using TCP packets.
+import socket
 
+def startClient():
+    host = '10.0.2.15'
+    port = 5000
+
+    s = socket.socket() #by default, the socket constructor creates an TCP/IPv4 socket
+    s.connect((host,port))
+
+    message = input("-> ")
+    while message != 'q':
+        s.send(message.encode('utf-8')) 
+        #1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+        data = s.recv(1024).decode('utf-8') 
+        print(data)
+        message = input("-> ")
+    s.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
 if __name__ == '__main__':
+    startClient()
--- ee250/lab03/part3/ledServer.py
+++ ee250/lab03/part3/ledServer.py
@@ -4,11 +4,44 @@
 # the LED via TCP packets.
 
 import sys
+import socket
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
 sys.path.append('../../../Software/Python/')
 
 import grovepi
 
+def startServer():
+	# LED connected to D4
+	led = 4
+	grovepi.pinMode(led,"OUTPUT")
+	time.sleep(1)
+
+    host = '192.168.1.20'
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host,port))
+    s.listen(1)
+    c, addr = s.accept()
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        msg = ""
+        if not data:
+            break
+        print("VM: "+data)
+		if(data == "LED_ON")
+			grovepi.digitalWrite(led,1)
+			msg = "LED_ON Success"
+		else if(data == "LED_OFF")
+        	grovepi.digitalWrite(led,0)
+        	msg = "LED_OFF Success"
+        else
+        	msg = "Command Not Recognized"
+		print("-> "+msg)
+        c.send(msg.encode('utf-8'))
+    c.close()
+
 if __name__ == '__main__':
+    startServer()
     
