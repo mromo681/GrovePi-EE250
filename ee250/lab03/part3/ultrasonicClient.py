e07c264199e44f1a9b3df747344cd96b5a09a52d
Jeffrey Loh <jeffrezl@usc.edu>
2018-09-14 16:33:02 -0700

    changed ultrasonic stuff

--- ee250/lab03/part3/ultrasonicClient.py
+++ ee250/lab03/part3/ultrasonicClient.py
@@ -9,7 +9,40 @@ import sys
 # we are able to successfully `import grovepi`
 sys.path.append('../../../Software/Python/')
 
+# This append is to support importing the LCD library.
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+
 import grovepi
 
+import socket
+
+def Main():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '192.168.1.20'
+    port = 5001
+
+    server_addr = '192.168.0.147'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    s.bind((host,port))
+
+    # UDP is connectionless, so a client does not formally connect to a server
+    # before sending a message.
+    dst_port = 8050
+    while True:
+        #tuples are immutable so we need to overwrite the last tuple
+        server = (server_addr, int(dst_port))
+
+        ultrasonic = grovepi.ultrasonicRead(ultrasonic_ranger)
+
+        # for UDP, sendto() and recvfrom() are used instead
+        s.sendto(ultrasonic.encode('utf-8'), server) 
+        print("RPi: " + ultrasonic + " cm")
+        dst_port = input("destination port-> ")
+        time.sleep(0.2)
+    s.close()
+
 if __name__ == '__main__':
+	Main()
     
--- ee250/lab03/part3/ultrasonicServer.py
+++ ee250/lab03/part3/ultrasonicServer.py
@@ -3,5 +3,25 @@
 # This code runs on your VM and receives a stream of packets holding ultrasonic
 # sensor data and prints it to stdout. Use a UDP socket here.
 
+import socket
+
+def Process1():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '10.0.2.15'
+    port = 9000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host,port))
+
+    #print("Process 1 Server Started")
+    while True:
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print("VM: " + data + " cm")
+    c.close()
+
+
 if __name__ == '__main__':
+	Process1()
     
